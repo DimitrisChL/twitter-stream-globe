3e2582eec50f70f4de57d87be1b92b0dec5e2a9a
DimitrisChL <tseleisop@gmail.com>
2017-06-09 11:19:21 +0300

    Create TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -18,17 +18,17 @@ TweetBeacon = function(tweet) {
   this.color = 0xFFFFFF;
 
   if (tweet.sentiment.score < -2) {
-  this.color = 0x123554;
+  this.color = 0xe63f01;
   }
   else if (tweet.sentiment.score > 2) {
-    this.color = 0xafeeee;
+    this.color = 0x01f9ea;
   }
   else if(tweet.sentiment.score < 0 & tweet.sentiment.score > -2){
-    this.color = 0xfe6b7a;
+    this.color = 0xffd966;
   
   }
     else if(tweet.sentiment.score > 0 & twwet.sentiment.score > 2){
-    this.color = 0xee1f34;
+    this.color = 0x26ff01;
     }
 
   this.addBeam();
